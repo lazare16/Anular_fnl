64849397df130fc19e98703e79df6bb5078bd209
lazare16 <76008900+lazare16@users.noreply.github.com>
2025-01-21 23:51:06 +0400

    changing routing

--- angular-store/src/app/app.routes.ts
+++ angular-store/src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { AdminComponent } from './pages/admin/admin.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent },
-    { path: 'login', component: RegisterComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'admin', component: AdminComponent },
     { path: '**', redirectTo: '' }
